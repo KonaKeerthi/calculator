fa4e125e49fc59f62bb784bf3ce4cad7ebecbdaf
KonaKeerthi <141800919+KonaKeerthi@users.noreply.github.com>
2023-10-05 11:44:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const buttonsE1=document.querySelectorAll("button");
+const input=document.getElementById("result");
+
+for(let i=0;i<buttonsE1.length;i++)
+{
+    buttonsE1[i].addEventListener("click", () => {
+        const buttonValue = buttonsE1[i].textContent;
+        if(buttonValue === "AC")
+        {
+            clearResult();
+
+        }
+        else if(buttonValue === "=")
+        {
+            calculateResult();
+        }
+       
+        else{
+            appendValue(buttonValue);
+        }
+
+    });
+}
+function clearResult()
+{
+    input.value= "";
+}
+function calculateResult()
+{
+    input.value=eval(input.value);
+}
+function appendValue(buttonValue)
+{
+    input.value+=buttonValue;
+}
